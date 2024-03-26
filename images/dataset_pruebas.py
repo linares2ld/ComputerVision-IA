572158ed3852cfacc9c150fb8188cafca626345c
linares2ld <diego22.blng@gmail.com>
2024-03-26 10:10:40 -0600

    Se agregó un modulo en "images" el cual contiene las imagenes de prueba.

---
+++ images/dataset_pruebas.py
@@ -0,0 +1,6 @@
+import cv2
+
+Lechuga = cv2.imread("images/Lechuga.jpg")
+Lena = cv2.imread("images/Lena.jpg")
+Rayas_verticales = cv2.imread("images/Rayas_verticales.png")
+Tablero_ajedrez = cv2.imread("images/Tablero_ajedrez.jpg")
